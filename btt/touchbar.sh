e3ff9facb75384ac3d5d8a93a2b0c9ef79817a1a
Swapnil Agarwal <hi+github@swapnil.net>
2019-06-28 09:17:15 +0530

    update townscript event

--- btt/touchbar.sh
+++ btt/touchbar.sh
@@ -3,7 +3,7 @@ if curl -sL --fail http://google.com -o /dev/null; then
   curl -H "$(cat ~/Downloads/btt/github)" https://api.github.com/users/swapagarwal | /usr/local/bin/jq .followers > ~/Downloads/btt/github.out
   curl https://cdn.syndication.twimg.com/widgets/followbutton/info.json\?screen_names\=SwapAgarwal | /usr/local/bin/jq '.[0].followers_count' > ~/Downloads/btt/twitter.out
   curl -H "$(cat ~/Downloads/btt/github)" https://api.github.com/repos/swapagarwal/swag-for-dev | /usr/local/bin/jq .stargazers_count > ~/Downloads/btt/swag.out
-  curl -H "$(cat ~/Downloads/btt/townscript)" https://www.townscript.com/api/registration/getRegisteredUsers\?eventCode\=vueblr-15 | /usr/local/bin/jq -r '.data' | /usr/local/bin/jq '. | length' > ~/Downloads/btt/townscript.out
+  curl -H "$(cat ~/Downloads/btt/townscript)" https://www.townscript.com/api/registration/getRegisteredUsers\?eventCode\=vueblr-16 | /usr/local/bin/jq -r '.data' | /usr/local/bin/jq '. | length' > ~/Downloads/btt/townscript.out
 else
   echo "Unable to connect to the internet!"
 fi

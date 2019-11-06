9c17b013554168d22d024e22531ae7c71fb73c6d
Swapnil Agarwal <hi+github@swapnil.net>
2019-11-06 13:41:20 +0530

    move to dropbox

--- btt/touchbar.sh
+++ btt/touchbar.sh
@@ -1,9 +1,9 @@
 if curl -sL --fail http://google.com -o /dev/null; then
-  curl -H "$(cat ~/Downloads/btt/buttondown)" https://api.buttondown.email/v1/subscribers | /usr/local/bin/jq .count > ~/Downloads/btt/buttondown.out
-  curl -H "$(cat ~/Downloads/btt/github)" https://api.github.com/users/swapagarwal | /usr/local/bin/jq .followers > ~/Downloads/btt/github.out
-  curl https://cdn.syndication.twimg.com/widgets/followbutton/info.json\?screen_names\=SwapAgarwal | /usr/local/bin/jq '.[0].followers_count' > ~/Downloads/btt/twitter.out
-  curl -H "$(cat ~/Downloads/btt/github)" https://api.github.com/repos/swapagarwal/swag-for-dev | /usr/local/bin/jq .stargazers_count > ~/Downloads/btt/swag.out
-  curl -H "$(cat ~/Downloads/btt/townscript)" https://www.townscript.com/api/registration/getRegisteredUsers\?eventCode\=vueblr-19 | /usr/local/bin/jq -r '.data' | /usr/local/bin/jq '. | length' > ~/Downloads/btt/townscript.out
+  curl -H "$(cat ~/Dropbox/btt/buttondown)" https://api.buttondown.email/v1/subscribers | /usr/local/bin/jq .count > ~/Dropbox/btt/buttondown.out
+  curl -H "$(cat ~/Dropbox/btt/github)" https://api.github.com/users/swapagarwal | /usr/local/bin/jq .followers > ~/Dropbox/btt/github.out
+  curl https://cdn.syndication.twimg.com/widgets/followbutton/info.json\?screen_names\=SwapAgarwal | /usr/local/bin/jq '.[0].followers_count' > ~/Dropbox/btt/twitter.out
+  curl -H "$(cat ~/Dropbox/btt/github)" https://api.github.com/repos/swapagarwal/swag-for-dev | /usr/local/bin/jq .stargazers_count > ~/Dropbox/btt/swag.out
+  curl -H "$(cat ~/Dropbox/btt/townscript)" https://www.townscript.com/api/registration/getRegisteredUsers\?eventCode\=vueblr-20 | /usr/local/bin/jq -r '.data' | /usr/local/bin/jq '. | length' > ~/Dropbox/btt/townscript.out
 else
   echo "Unable to connect to the internet!"
 fi

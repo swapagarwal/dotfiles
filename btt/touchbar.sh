0f57011b1c0ff5fb0cb6c875debac34592af1d3f
Swapnil Agarwal <hi+github@swapnil.net>
2019-12-15 00:29:27 +0530

    untrack townscript

--- btt/touchbar.sh
+++ btt/touchbar.sh
@@ -4,7 +4,6 @@ if curl -sL --fail http://google.com -o /dev/null; then
   curl https://cdn.syndication.twimg.com/widgets/followbutton/info.json\?screen_names\=SwapAgarwal | /usr/local/bin/jq '.[0].followers_count' > ~/Dropbox/btt/twitter.out
   curl https://cdn.syndication.twimg.com/widgets/followbutton/info.json\?screen_names\=DXClubIO | /usr/local/bin/jq '.[0].followers_count' > ~/Dropbox/btt/dxclub.out
   curl -H "$(cat ~/Dropbox/btt/github)" https://api.github.com/repos/swapagarwal/swag-for-dev | /usr/local/bin/jq .stargazers_count > ~/Dropbox/btt/swag.out
-  curl -H "$(cat ~/Dropbox/btt/townscript)" https://www.townscript.com/api/registration/getRegisteredUsers\?eventCode\=vueblr-20 | /usr/local/bin/jq -r '.data' | /usr/local/bin/jq '. | length' > ~/Dropbox/btt/townscript.out
 else
   echo "Unable to connect to the internet!"
 fi

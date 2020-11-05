b1ab3b8058379b0c2fc0cb348b96fb7f001a3be8
Swapnil Agarwal <hi+github@swapnil.net>
2020-11-05 11:48:29 +0530

    untrack github

--- btt/touchbar.sh
+++ btt/touchbar.sh
@@ -1,6 +1,6 @@
 if curl -sL --fail http://google.com -o /dev/null; then
   curl -H "$(cat ~/Dropbox/btt/buttondown)" https://api.buttondown.email/v1/subscribers | /usr/local/bin/jq .count > ~/Dropbox/btt/buttondown.out
-  curl -H "$(cat ~/Dropbox/btt/github)" https://api.github.com/users/swapagarwal | /usr/local/bin/jq .followers > ~/Dropbox/btt/github.out
+  # curl -H "$(cat ~/Dropbox/btt/github)" https://api.github.com/users/swapagarwal | /usr/local/bin/jq .followers > ~/Dropbox/btt/github.out
   curl https://cdn.syndication.twimg.com/widgets/followbutton/info.json\?screen_names\=SwapAgarwal | /usr/local/bin/jq '.[0].followers_count' > ~/Dropbox/btt/twitter.out
   curl https://cdn.syndication.twimg.com/widgets/followbutton/info.json\?screen_names\=DXClubIO | /usr/local/bin/jq '.[0].followers_count' > ~/Dropbox/btt/dxclub.out
   curl https://cdn.syndication.twimg.com/widgets/followbutton/info.json\?screen_names\=ReademyHQ | /usr/local/bin/jq '.[0].followers_count' > ~/Dropbox/btt/reademy.out

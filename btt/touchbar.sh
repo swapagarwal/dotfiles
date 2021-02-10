575c7e8afdfaf4799589c3841aa5cb9362c7c851
Swapnil Agarwal <hi+github@swapnil.net>
2021-02-10 13:58:25 +0530

    update btt location

--- btt/touchbar.sh
+++ btt/touchbar.sh
@@ -1,11 +1,11 @@
 if curl -sL --fail http://google.com -o /dev/null; then
-  curl -H "$(cat ~/Dropbox/btt/buttondown)" https://api.buttondown.email/v1/subscribers | /usr/local/bin/jq .count > ~/Dropbox/btt/buttondown.out
-  # curl -H "$(cat ~/Dropbox/btt/github)" https://api.github.com/users/swapagarwal | /usr/local/bin/jq .followers > ~/Dropbox/btt/github.out
-  curl https://cdn.syndication.twimg.com/widgets/followbutton/info.json\?screen_names\=SwapAgarwal | /usr/local/bin/jq '.[0].followers_count' > ~/Dropbox/btt/twitter.out
-  curl https://cdn.syndication.twimg.com/widgets/followbutton/info.json\?screen_names\=DX_club_ | /usr/local/bin/jq '.[0].followers_count' > ~/Dropbox/btt/dxclub.out
-  curl https://cdn.syndication.twimg.com/widgets/followbutton/info.json\?screen_names\=ReademyHQ | /usr/local/bin/jq '.[0].followers_count' > ~/Dropbox/btt/reademy.out
-  # curl -H "$(cat ~/Dropbox/btt/github)" https://api.github.com/repos/swapagarwal/swag-for-dev | /usr/local/bin/jq .stargazers_count > ~/Dropbox/btt/swag.out
-  curl https://emailoctopus.com/api/1.5/lists/"$(cat ~/Dropbox/btt/emailoctopus)" | /usr/local/bin/jq '.counts.subscribed' > ~/Dropbox/btt/emailoctopus.out
+  curl -H "$(cat ~/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/btt/buttondown)" https://api.buttondown.email/v1/subscribers | /usr/local/bin/jq .count > ~/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/btt/buttondown.out
+  # curl -H "$(cat ~/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/btt/github)" https://api.github.com/users/swapagarwal | /usr/local/bin/jq .followers > ~/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/btt/github.out
+  curl https://cdn.syndication.twimg.com/widgets/followbutton/info.json\?screen_names\=SwapAgarwal | /usr/local/bin/jq '.[0].followers_count' > ~/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/btt/twitter.out
+  curl https://cdn.syndication.twimg.com/widgets/followbutton/info.json\?screen_names\=DX_club_ | /usr/local/bin/jq '.[0].followers_count' > ~/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/btt/dxclub.out
+  curl https://cdn.syndication.twimg.com/widgets/followbutton/info.json\?screen_names\=ReademyHQ | /usr/local/bin/jq '.[0].followers_count' > ~/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/btt/reademy.out
+  # curl -H "$(cat ~/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/btt/github)" https://api.github.com/repos/swapagarwal/swag-for-dev | /usr/local/bin/jq .stargazers_count > ~/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/btt/swag.out
+  curl https://emailoctopus.com/api/1.5/lists/"$(cat ~/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/btt/emailoctopus)" | /usr/local/bin/jq '.counts.subscribed' > ~/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/btt/emailoctopus.out
 else
   echo "Unable to connect to the internet!"
 fi

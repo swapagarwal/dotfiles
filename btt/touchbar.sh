54af2e646c7eadd7066671c8105b6d12571e5ac7
Swapnil Agarwal <hi+github@swapnil.net>
2021-02-26 09:47:53 +0530

    track convertkit

--- btt/touchbar.sh
+++ btt/touchbar.sh
@@ -6,6 +6,7 @@ if curl -sL --fail http://google.com -o /dev/null; then
   curl https://cdn.syndication.twimg.com/widgets/followbutton/info.json\?screen_names\=ReademyHQ | /usr/local/bin/jq '.[0].followers_count' > ~/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/btt/reademy.out
   # curl -H "$(cat ~/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/btt/github)" https://api.github.com/repos/swapagarwal/swag-for-dev | /usr/local/bin/jq .stargazers_count > ~/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/btt/swag.out
   curl https://emailoctopus.com/api/1.5/lists/"$(cat ~/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/btt/emailoctopus)" | /usr/local/bin/jq '.counts.subscribed' > ~/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/btt/emailoctopus.out
+  curl https://api.convertkit.com/v3/subscribers\?api_secret="$(cat ~/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/btt/convertkit)" | /usr/local/bin/jq '.total_subscribers' > ~/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/btt/convertkit.out
 else
   echo "Unable to connect to the internet!"
 fi
